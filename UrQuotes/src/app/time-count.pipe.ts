9fa5b68f9ecce79ffed325e97a2114a10a2a194f
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-07-20 12:33:30 +0300

    add card styling to individual quotes.

--- UrQuotes/src/app/time-count.pipe.ts
+++ UrQuotes/src/app/time-count.pipe.ts
@@ -10,9 +10,9 @@ export class TimeCountPipe implements PipeTransform {
   let today: Date = new Date();
   let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
   var dateDifference = Math.abs(todayWithNoTime - value)
-  const secondsinDay = 86400;
+  const secondsInDay = 86400;
   var dateDifferenceSeconds = dateDifference*0.001;
-  var timeCounter = dateDifferenceSeconds/secondsinDay;
+  var timeCounter = dateDifferenceSeconds/secondsInDay;
   
   if(timeCounter >= 1 && value < todayWithNoTime){
     return timeCounter;
